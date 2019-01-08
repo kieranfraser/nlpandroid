778fad822a19bd429826585578d1d9399bef33e9
Kieran Fraser <kfraser@tcd.ie>
2019-01-08 18:54:25 +0000

    Updated entity

--- nlpandroid/src/main/kotlin/com/aempathy/NLPAndroid/models/Entity.kt
+++ nlpandroid/src/main/kotlin/com/aempathy/NLPAndroid/models/Entity.kt
@@ -3,6 +3,6 @@ package com.aempathy.NLPAndroid.models
 import com.google.gson.annotations.SerializedName
 
 data class Entity (
-    @SerializedName("entity") val entity: String,
-    @SerializedName("type") val type: String
+    @SerializedName("entity") val entity: String ?= "",
+    @SerializedName("type") val type: String ?= ""
 )
